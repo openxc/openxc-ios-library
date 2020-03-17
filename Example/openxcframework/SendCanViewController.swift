a7d489eff2515a95d7e7dde0c54cb0a177936f41
kranjanford <kranjan@ford.com>
2020-03-17 19:19:39 +0530

    Added the lower case entry

--- Example/openxcframework/SendCanViewController.swift
+++ Example/openxcframework/SendCanViewController.swift
@@ -261,7 +261,7 @@ extension String {
 
 var containsValidCharacter: Bool {
     guard self != "" else { return true }
-    let hexSet = CharacterSet(charactersIn: "1234567890ABCDEF")
+    let hexSet = CharacterSet(charactersIn: "1234567890ABCDEFabcdef")
     let newSet = CharacterSet(charactersIn: self)
     return hexSet.isSuperset(of: newSet)
   }
